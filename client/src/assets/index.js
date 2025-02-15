a8a04add1fee471361358c2414ec53b682497e32
sulthanularief148 <sulthanularief148@gmail.com>
2025-02-15 23:36:03 +0530

    logo added in client

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -70,6 +70,7 @@ import contact_img from './contact_img.png'
 import razorpay_logo from './razorpay_logo.png'
 import stripe_logo from './stripe_logo.png'
 import cross_icon from './cross_icon.png'
+import logo from "./logo.png"
 
 export const assets = {
     logo,
