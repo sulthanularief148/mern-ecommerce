25a21fd836eff381c7d27bd091b2117aea708a1f
Gulam <sulthanularief148@gmail.com>
2025-10-01 23:50:05 +0530

    ToggleText component updated

--- client/src/components/ToggleText.jsx
+++ client/src/components/ToggleText.jsx
@@ -1,20 +1,19 @@
-const ToggleText = ({ currenState, setCurrentState, loginContent }) => (
-    <div className="w-full flex justify-between text-sm mt-[-8px]">
-      <p className="cursor-pointer">{loginContent.forgotPassword}</p>
-      {currenState === "Login" ? (
-        <p onClick={() => setCurrentState("Sign Up")} className="cursor-pointer">
-          {loginContent.createAccount}
-        </p>
-      ) : (
-        <p
-          className="cursor-pointer"
-          onClick={() => setCurrentState("Login")}
-        >
-          {loginContent.login}
-        </p>
-      )}
-    </div>
-  );
-  
-  export default ToggleText;
-  
+const ToggleText = ({ currentState, setCurrentState, loginContent }) => (
+  <div className="w-full flex justify-between text-sm mt-[-8px]">
+    <p className="cursor-pointer">{loginContent.forgotPassword}</p>
+    {currentState === "Login" ? (
+      <p onClick={() => setCurrentState("Sign Up")} className="cursor-pointer">
+        {loginContent.createAccount}
+      </p>
+    ) : (
+      <p
+        className="cursor-pointer"
+        onClick={() => setCurrentState("Login")}
+      >
+        {loginContent.login}
+      </p>
+    )}
+  </div>
+);
+
+export default ToggleText;
